c4588012f736c0cc0ce96372ddf3be8e404bfcf0
924346912@qq.com <wkyhy@qq.com>
2019-02-20 15:17:48 +0800

    优化

--- js/util.js
+++ js/util.js
@@ -2,29 +2,35 @@
 	_mui('.mui-scroll-wrapper').scroll({
 		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 	});
-	return pullRefresh;
+	return $util;
 })(mui);
 
-function pullRefresh(dom,callback){
-	mui.init({
-	  pullRefresh : {
-		container:dom,//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
-		up : {
-		  height:50,//可选.默认50.触发上拉加载拖动距离
-		  auto:false,//可选,默认false.自动上拉加载一次
-		  contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
-		  contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
-		  callback :function(){
-			  callback(function(){
-				  this.endPullupToRefresh(false);
-			  }.bind(this));
-			  //this.endPullupToRefresh(false);
-		  } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-		}
-	  }
-	});
+var $util={
+	//上啦加载
+	pullRefresh:function(dom,callback){
+		mui.init({
+		  pullRefresh : {
+			container:dom,//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+			up : {
+			  height:50,//可选.默认50.触发上拉加载拖动距离
+			  auto:false,//可选,默认false.自动上拉加载一次
+			  contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
+			  contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+			  callback :function(){
+				  callback(function(){
+					  this.endPullupToRefresh(false);
+				  }.bind(this));
+				  //this.endPullupToRefresh(false);
+			  } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+			}
+		  }
+		});
+	},
+	//弹窗
+	dialog(options){
+		var __popup__=document.createElement('div');
+		__popup__.setAttribute('class','mui-popup');
+		document.body.appendChild(__popup__);
+	}
 }
 
-function tt(a){
-	console.log(a)
-}
